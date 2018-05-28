47d1b2a23edd67b10fad2dc0d22737683f86c544
thegeekkid <brian@bscc.support>
2018-05-27 19:42:01 -0500

    Initial commit - initial logic laid down

--- ProductionReadyService/Service1.cs
+++ ProductionReadyService/Service1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Win32;
 
 namespace ProductionReadyService
 {
     public partial class Service1 : ServiceBase
     {
+       
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +22,69 @@ namespace ProductionReadyService
 
         protected override void OnStart(string[] args)
         {
+            Thread trd = new Thread(runner);
+            trd.IsBackground = true;
+            trd.Start();
         }
 
         protected override void OnStop()
         {
+            Process proc = new Process();
+            proc.StartInfo.FileName = @"C:\Windows\System32\net.exe";
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.StartInfo.Arguments = @"start wuauserv_prod";
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        protected void runner()
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = @"C:\Windows\System32\net.exe";
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            do
+            {
+                if (pmode)
+                {
+                    if (Days.Contains(((int)DateTime.Now.DayOfWeek).ToString()))
+                    {
+                        proc.StartInfo.Arguments = @"stop wuauserv_prod";
+                        proc.Start();
+                        proc.WaitForExit();
+                    }else
+                    {
+                        proc.StartInfo.Arguments = @"start wuauserv_prod";
+                        proc.Start();
+                        proc.WaitForExit();
+                    }
+                }else
+                {
+                    proc.StartInfo.Arguments = @"start wuauserv_prod";
+                    proc.Start();
+                    proc.WaitForExit();
+                }
+                // Sleep for 15 minutes
+                Thread.Sleep(900000);
+            } while (1 == 1);
+        }
+
+        public bool pmode = false;
+        public string Days = "";
+
+        protected void readSettings()
+        {
+            RegistryKey root = Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Semrau Software Consulting").OpenSubKey("ProductionReady");
+
+            pmode = bool.Parse(root.GetValue("pmode").ToString());
+
+            Days = root.GetValue("ProductionDays").ToString();
+            
+            if (Days.Split(',').Count() > 3)
+            {
+                pmode = false;
+            }
         }
     }
 }
