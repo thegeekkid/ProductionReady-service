bcb1c8df15917a60fc7316d441a86e6797e0e4ae
thegeekkid <brian@bscc.support>
2018-05-27 20:51:22 -0500

    Finished Service

--- ProductionReadyService/Service1.cs
+++ ProductionReadyService/Service1.cs
@@ -29,62 +29,153 @@ namespace ProductionReadyService
 
         protected override void OnStop()
         {
-            Process proc = new Process();
-            proc.StartInfo.FileName = @"C:\Windows\System32\net.exe";
-            proc.StartInfo.CreateNoWindow = true;
-            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            proc.StartInfo.Arguments = @"start wuauserv_prod";
-            proc.Start();
-            proc.WaitForExit();
+            EnableUpdateSvc();
         }
 
         protected void runner()
         {
-            Process proc = new Process();
-            proc.StartInfo.FileName = @"C:\Windows\System32\net.exe";
-            proc.StartInfo.CreateNoWindow = true;
-            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
             do
             {
-                if (pmode)
+                readSettings();
+                if (DisableUpdates)
                 {
-                    if (Days.Contains(((int)DateTime.Now.DayOfWeek).ToString()))
+                    if (pmode)
                     {
-                        proc.StartInfo.Arguments = @"stop wuauserv_prod";
-                        proc.Start();
-                        proc.WaitForExit();
-                    }else
+                        if (Days.Contains(((int)DateTime.Now.DayOfWeek).ToString()))
+                        {
+                            DisableUpdateSvc();
+                        }
+                        else
+                        {
+                            EnableUpdateSvc();
+                        }
+                    }
+                    else
                     {
-                        proc.StartInfo.Arguments = @"start wuauserv_prod";
-                        proc.Start();
-                        proc.WaitForExit();
+                        EnableUpdateSvc();
                     }
                 }else
                 {
-                    proc.StartInfo.Arguments = @"start wuauserv_prod";
-                    proc.Start();
-                    proc.WaitForExit();
+                    EnableUpdateSvc();
                 }
-                // Sleep for 15 minutes
-                Thread.Sleep(900000);
+                
+                // Sleep for 1 hour
+                Thread.Sleep(3600000);
             } while (1 == 1);
         }
 
         public bool pmode = false;
+        public bool DisableUpdates = false;
         public string Days = "";
 
         protected void readSettings()
         {
             RegistryKey root = Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Semrau Software Consulting").OpenSubKey("ProductionReady");
 
-            pmode = bool.Parse(root.GetValue("pmode").ToString());
+            pmode = bool.Parse(root.GetValue("pmode", "False").ToString());
 
-            Days = root.GetValue("ProductionDays").ToString();
+            DisableUpdates = bool.Parse(root.GetValue("DisableUpdates", "False").ToString());
+
+            Days = root.GetValue("ProductionDays", "").ToString();
             
             if (Days.Split(',').Count() > 3)
             {
                 pmode = false;
             }
         }
+
+        protected void DisableUpdateSvc()
+        {
+            if (Registry.GetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\wuauserv", "ImagePath", null) != null)
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = @"C:\Windows\System32\net.exe";
+                proc.StartInfo.Arguments = @"stop wuauserv";
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.Start();
+                proc.WaitForExit();
+                RenameSubKey(Registry.LocalMachine.OpenSubKey("SYSTEM").OpenSubKey("ControlSet001").OpenSubKey("Services", true), "wuauserv", "wuauserv-prod");
+            }
+        }
+        protected void EnableUpdateSvc()
+        {
+            if (Registry.GetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\wuauserv-prod", "ImagePath", null) != null)
+            {
+                RenameSubKey(Registry.LocalMachine.OpenSubKey("SYSTEM").OpenSubKey("ControlSet001").OpenSubKey("Services", true), "wuauserv-prod", "wuauserv");
+                Process proc = new Process();
+                proc.StartInfo.FileName = @"C:\Windows\System32\net.exe";
+                proc.StartInfo.Arguments = @"start wuauserv";
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.Start();
+                proc.WaitForExit();
+            }
+        }
+
+
+
+        /// <summary>
+        /// Renames a subkey of the passed in registry key since 
+        /// the Framework totally forgot to include such a handy feature.
+        /// </summary>
+        /// <param name="regKey">The RegistryKey that contains the subkey 
+        /// you want to rename (must be writeable)</param>
+        /// <param name="subKeyName">The name of the subkey that you want to rename
+        /// </param>
+        /// <param name="newSubKeyName">The new name of the RegistryKey</param>
+        /// <returns>True if succeeds</returns>
+        public bool RenameSubKey(RegistryKey parentKey,
+            string subKeyName, string newSubKeyName)
+        {
+            CopyKey(parentKey, subKeyName, newSubKeyName);
+            parentKey.DeleteSubKeyTree(subKeyName);
+            return true;
+        }
+
+        /// <summary>
+        /// Copy a registry key.  The parentKey must be writeable.
+        /// </summary>
+        /// <param name="parentKey"></param>
+        /// <param name="keyNameToCopy"></param>
+        /// <param name="newKeyName"></param>
+        /// <returns></returns>
+        public bool CopyKey(RegistryKey parentKey,
+            string keyNameToCopy, string newKeyName)
+        {
+            //Create new key
+            RegistryKey destinationKey = parentKey.CreateSubKey(newKeyName);
+
+            //Open the sourceKey we are copying from
+            RegistryKey sourceKey = parentKey.OpenSubKey(keyNameToCopy);
+
+            RecurseCopyKey(sourceKey, destinationKey);
+
+            return true;
+        }
+
+        private void RecurseCopyKey(RegistryKey sourceKey, RegistryKey destinationKey)
+        {
+            //copy all the values
+            foreach (string valueName in sourceKey.GetValueNames())
+            {
+                object objValue = sourceKey.GetValue(valueName);
+                RegistryValueKind valKind = sourceKey.GetValueKind(valueName);
+                destinationKey.SetValue(valueName, objValue, valKind);
+            }
+
+            //For Each subKey 
+            //Create a new subKey in destinationKey 
+            //Call myself 
+            foreach (string sourceSubKeyName in sourceKey.GetSubKeyNames())
+            {
+                RegistryKey sourceSubKey = sourceKey.OpenSubKey(sourceSubKeyName);
+                RegistryKey destSubKey = destinationKey.CreateSubKey(sourceSubKeyName);
+                RecurseCopyKey(sourceSubKey, destSubKey);
+            }
+        }
     }
+
+    
 }
